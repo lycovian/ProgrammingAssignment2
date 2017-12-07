729f4f629613ca905dc766d357062f2f3b6d0855
Mike Wilson <mwilson@whalerockindustries.com>
2017-12-07 09:04:17 -0800

    Assignment submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions work with a memoized matrix inversion
 
-## Write a short comment describing this function
-
+## Definition of memoized matrix inversion
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+    }
+    get <- function() x
+    setinv <- function(i) inv <<- i
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Memoized matrix solve().  Uses cached matrix inversion if exists.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("INFO: Returning cached inverted matrix.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
+
+## Tests
+message("Testing memoized matrix\n=============")
+message("Create random memoized matrix ...")
+m <- matrix(ceiling(sapply(runif(9), function(x) x*10)), nrow=3)
+cm <- makeCacheMatrix(m)
+print(cm$get())
+message("Solve memoized matrix ...")
+cs <- cacheSolve(cm)
+print(cs)
+message("Re-run solve ...")
+cs <- cacheSolve(cm)
+print(cs)
